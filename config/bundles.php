f851dbfdb5bf2b3a6d0d5674e018dc65ece5ae82
Viacheslav Dymarchuk <sl.lubart@gmail.com>
2024-01-16 21:56:25 +0100

    fix: Fix OAuth installation

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     WouterJ\EloquentBundle\WouterJEloquentBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    League\Bundle\OAuth2ServerBundle\LeagueOAuth2ServerBundle::class => ['all' => true],
 ];
